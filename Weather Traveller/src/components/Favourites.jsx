028d017c1b20d614ec1b5357a4efd3076910cec6
Your Name <matlalaj67@gmail.com>
2025-01-15 11:12:36 +0200

    Updated Delete button functionality to delete user saved location

--- Weather Traveller/src/components/Favourites.jsx
+++ Weather Traveller/src/components/Favourites.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React , { useState } from 'react';
+import { useLocation,useNavigate } from 'react-router-dom';
 import './Favourites.css';
 
 const Favourites = () => {
   const location = useLocation();
-  const favourites = location.state?.favourites || [];
+  const navigate = useNavigate();
+  const [favourites, setFavourites] = useState(location.state?.favourites || []);
+ 
+  const handleDelete = (index) => {
+     const updatedFavourites = favourites.filter((_, i) => i !== index);
+      setFavourites(updatedFavourites); 
+  navigate('/favourites', { state: { favourites: updatedFavourites } }); };
+
 
   return (
     <div className="Favourites">
       <h1>Favourite Locations</h1>
       <ul>
         {favourites.map((fav, index) => (
-          <li key={index}>{fav}</li>
+          <li key={index}>
+            {fav}
+            <button onClick={() => handleDelete(index)} className="deleteButton">Delete</button>            </li>
         ))}
       </ul>
     </div>
